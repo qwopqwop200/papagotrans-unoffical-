7d343cd510ecb4e1d8369dd940b3bc62976c1a8a
qwopqwop200 <64115820+qwopqwop200@users.noreply.github.com>
2021-08-12 23:45:33 +0900

    Update papagotrans.py
    remove class

--- papagotrans.py
+++ papagotrans.py
@@ -5,18 +5,6 @@ import threading
 import chromedriver_autoinstaller
 from selenium import webdriver
 
-class AnyEc:
-    def __init__(self, *args):
-        self.ecs = args
-    def __call__(self, driver):
-        for fn in self.ecs:
-            try:
-                res = fn(driver)
-                if res:
-                    return True
-            except:
-                pass
-
 class Translated:
     def __init__(self,source,target,origin,text,pronunciation):
         self.source = source
